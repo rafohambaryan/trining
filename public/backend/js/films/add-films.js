d51f5a5caabca407a306c1f2a02d670d9c371be6
rafohambaryan <raforafo2014@yandex.ru>
2020-06-04 21:43:18 +0400

    fix

--- public/backend/js/films/add-films.js
+++ public/backend/js/films/add-films.js
@@ -73,15 +73,22 @@ function printCheckedToms(res, code) {
 }
 
 var app = new Map();
+var click = true;
 $(document).ready(function () {
 
     $('#dataTableFilmsList').DataTable();
     $(document).on('click', '.add-new-film', function () {
-        $('#exampleModalCenter .modal-body *').remove()
-        $('#exampleModalCenter .modal-footer').removeClass('d-none')
-        $('#exampleModalCenter #exampleModalLongTitle').text('New Film')
-        $('#exampleModalCenter').modal('show')
-        $('#exampleModalCenter .append-content-modal').append(addFilmContent())
+        if (click) {
+            click = false;
+            $('#exampleModalCenter .modal-body *').remove();
+            $('#exampleModalCenter .modal-footer').removeClass('d-none');
+            $('#exampleModalCenter #exampleModalLongTitle').text('New Film');
+            $('#exampleModalCenter').modal('show');
+            $('#exampleModalCenter .append-content-modal').append(addFilmContent());
+            setTimeout(function () {
+                click = true;
+            }, 300);
+        }
 
     });
     $(document).on('click', '.remove-date-film-content', function () {
@@ -89,55 +96,66 @@ $(document).ready(function () {
         main.remove()
     });
     $(document).on('click', '.add-new-date-line-film', function () {
-        $('#exampleModalCenter .modal-body .append-new-date-input').append(addNewDate())
+        if (click) {
+            click = false;
+            setTimeout(function () {
+                click = true;
+            }, 300);
+            $('#exampleModalCenter .modal-body .append-new-date-input').append(addNewDate())
+        }
     });
     $(document).on('click', '.click-save-add-or-update-film', function () {
-        let form = $('.form-update-or-create').serializeArray();
-        let valid = true;
-        let dateArr = [];
-        var formData = new FormData();
-        let film_id = 0;
-        $.each(form, function (i, item) {
-            switch (item.name) {
-                case 'name':
-                    formData.append('name', item.value)
-                    if (item.value.length === 0) {
-                        $(`input[name=${item.name}]`).addClass('add-error-border')
-                        valid = false
-                    } else {
-                        $(`input[name=${item.name}]`).removeClass('add-error-border')
-                    }
-                    break;
-                case 'description':
-                    formData.append('description', item.value)
-                    break;
-                case 'film_id':
-                    film_id = item.value
-                    break;
-            }
-            if (item.name === 'start_date[]' && form[i + 1].value.length > 0) {
-                dateArr.push(item.value + ' - ' + form[i + 1].value)
-            }
-        });
-        formData.append('date', JSON.stringify(dateArr));
-        if (valid) {
-            fetch(window.location.origin + '/admin/films/' + film_id, {
-                headers: {
-                    "X-CSRF-Token": $('meta[name="csrf_token"]').attr('content')
-                },
-                body: formData,
-                method: 'POST'
-            }).then(response => {
-                return response.json();
-            }).then((res) => {
-                if (res.success) {
-                    if (film_id > 0) {
-                        let content = $(`#dataTableFilmsList tbody .card-film-tr[data-id=${film_id}]`)
-                        content.addClass('bg-green')
-                        content.find('.name').text(res.data.name);
-                        content.find('.description').text(res.data.description ? res.data.description : '');
-                    } else {
-                        $('#dataTableFilmsList tbody').prepend(`<tr class="card-film-tr bg-green" data-id="${res.data.id}">
+        if (click) {
+            click = false;
+            setTimeout(function () {
+                click = true;
+            }, 300);
+            let form = $('.form-update-or-create').serializeArray();
+            let valid = true;
+            let dateArr = [];
+            var formData = new FormData();
+            let film_id = 0;
+            $.each(form, function (i, item) {
+                switch (item.name) {
+                    case 'name':
+                        formData.append('name', item.value)
+                        if (item.value.length === 0) {
+                            $(`input[name=${item.name}]`).addClass('add-error-border')
+                            valid = false
+                        } else {
+                            $(`input[name=${item.name}]`).removeClass('add-error-border')
+                        }
+                        break;
+                    case 'description':
+                        formData.append('description', item.value)
+                        break;
+                    case 'film_id':
+                        film_id = item.value
+                        break;
+                }
+                if (item.name === 'start_date[]' && form[i + 1].value.length > 0) {
+                    dateArr.push(item.value + ' - ' + form[i + 1].value)
+                }
+            });
+            formData.append('date', JSON.stringify(dateArr));
+            if (valid) {
+                fetch(window.location.origin + '/admin/films/' + film_id, {
+                    headers: {
+                        "X-CSRF-Token": $('meta[name="csrf_token"]').attr('content')
+                    },
+                    body: formData,
+                    method: 'POST'
+                }).then(response => {
+                    return response.json();
+                }).then((res) => {
+                    if (res.success) {
+                        if (film_id > 0) {
+                            let content = $(`#dataTableFilmsList tbody .card-film-tr[data-id=${film_id}]`)
+                            content.addClass('bg-green')
+                            content.find('.name').text(res.data.name);
+                            content.find('.description').text(res.data.description ? res.data.description : '');
+                        } else {
+                            $('#dataTableFilmsList tbody').prepend(`<tr class="card-film-tr bg-green" data-id="${res.data.id}">
                                                                 <td>${res.data.id}</td>
                                                                 <td class="name">${res.data.name}</td>
                                                                 <td class="description">${res.data.description ? res.data.description : ''}</td>
@@ -149,80 +167,98 @@ $(document).ready(function () {
                                                                     </div>
                                                                 </td>
                                                             </tr>`);
+                        }
+                        $('#exampleModalCenter').modal('hide');
+                        setTimeout(function () {
+                            $('#dataTableFilmsList tbody .card-film-tr').removeClass('bg-green');
+                        }, 1500)
                     }
-                    $('#exampleModalCenter').modal('hide');
-                    setTimeout(function () {
-                        $('#dataTableFilmsList tbody .card-film-tr').removeClass('bg-green');
-                    }, 1500)
+                });
+            }
+        }
+    });
+    $(document).on('click', '.delete-film', function () {
+        if (click) {
+            click = false;
+            setTimeout(function () {
+                click = true;
+            }, 300);
+            let parent = $(this).parents('.card-film-tr')
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.value) {
+                    fetch(window.location.origin + '/admin/films/delete', {
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json",
+                            "X-Requested-With": "XMLHttpRequest",
+                            "X-CSRF-Token": $('meta[name="csrf_token"]').attr('content')
+                        },
+                        body: JSON.stringify({film_id: parent.attr('data-id')}),
+                        method: 'delete'
+                    }).then(response => {
+                        return response.json();
+                    }).then((res) => {
+                        if (res.success) {
+                            parent.remove();
+                            Swal.fire({
+                                // position: 'top-end',
+                                icon: 'success',
+                                title: 'Your work has been deleted',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        }
+                    });
                 }
             });
         }
     });
-    $(document).on('click', '.delete-film', function () {
-        let parent = $(this).parents('.card-film-tr')
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.value) {
-                fetch(window.location.origin + '/admin/films/delete', {
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json",
-                        "X-Requested-With": "XMLHttpRequest",
-                        "X-CSRF-Token": $('meta[name="csrf_token"]').attr('content')
-                    },
-                    body: JSON.stringify({film_id: parent.attr('data-id')}),
-                    method: 'delete'
-                }).then(response => {
-                    return response.json();
-                }).then((res) => {
-                    if (res.success) {
-                        parent.remove();
-                        Swal.fire({
-                            // position: 'top-end',
-                            icon: 'success',
-                            title: 'Your work has been deleted',
-                            showConfirmButton: false,
-                            timer: 1500
-                        })
-                    }
-                });
-            }
-        });
-    });
     $(document).on('click', '.update-film', function () {
-        let parent = $(this).parents('.card-film-tr');
-        $('#exampleModalCenter .modal-body *').remove()
-        $('#exampleModalCenter .modal-footer').removeClass('d-none')
-        fetch(window.location.origin + '/admin/films/' + parent.attr('data-id')).then(response => {
-            return response.json();
-        }).then((res) => {
-            if (res.success) {
-                $('#exampleModalCenter #exampleModalLongTitle').html(`<span>Update <strong>(${res.data.name})</strong> Film</span>`)
-                $('#exampleModalCenter').modal('show')
-                $('#exampleModalCenter .append-content-modal').append(addFilmContent(res.data.name, res.data.description, res.data.get_date, res.data.id))
-            }
-        });
+        if (click) {
+            click = false;
+            setTimeout(function () {
+                click = true;
+            }, 300);
+            let parent = $(this).parents('.card-film-tr');
+            $('#exampleModalCenter .modal-body *').remove()
+            $('#exampleModalCenter .modal-footer').removeClass('d-none')
+            fetch(window.location.origin + '/admin/films/' + parent.attr('data-id')).then(response => {
+                return response.json();
+            }).then((res) => {
+                if (res.success) {
+                    $('#exampleModalCenter #exampleModalLongTitle').html(`<span>Update <strong>(${res.data.name})</strong> Film</span>`)
+                    $('#exampleModalCenter').modal('show')
+                    $('#exampleModalCenter .append-content-modal').append(addFilmContent(res.data.name, res.data.description, res.data.get_date, res.data.id))
+                }
+            });
+        }
 
     });
     $(document).on('click', '.get-checked-film', function () {
-        let parent = $(this).parents('.card-film-tr');
-        $('#exampleModalCenter .modal-body *').remove()
-        $('#exampleModalCenter .modal-footer').addClass('d-none')
-        fetch(window.location.origin + '/admin/films/' + parent.attr('data-id')).then(response => {
-            return response.json();
-        }).then((res) => {
-            if (res.success) {
-                if (res.data.get_date.length > 0) {
-                    $('#exampleModalCenter #exampleModalLongTitle').html(`<span>get checked film (${res.data.name}) </span>`);
-                    $('#exampleModalCenter').modal('show');
-                    $('#exampleModalCenter .append-content-modal').append(`<form class="date_form_checked_list">
+        if (click) {
+            click = false;
+            setTimeout(function () {
+                click = true;
+            }, 300);
+            let parent = $(this).parents('.card-film-tr');
+            $('#exampleModalCenter .modal-body *').remove()
+            $('#exampleModalCenter .modal-footer').addClass('d-none')
+            fetch(window.location.origin + '/admin/films/' + parent.attr('data-id')).then(response => {
+                return response.json();
+            }).then((res) => {
+                if (res.success) {
+                    if (res.data.get_date.length > 0) {
+                        $('#exampleModalCenter #exampleModalLongTitle').html(`<span>get checked film (${res.data.name}) </span>`);
+                        $('#exampleModalCenter').modal('show');
+                        $('#exampleModalCenter .append-content-modal').append(`<form class="date_form_checked_list">
                                                                                 <input type="hidden" id="set_date_id_admin" name="date_id" value="0">
                                                                                 <input type="hidden" id="set_film_id_admin" name="film_id" value="${res.data.id}">
                                                                            </form>
@@ -230,18 +266,19 @@ $(document).ready(function () {
                                                                                 <option disabled selected>selected Date</option>
                                                                            </select>
                                                                            <table class="costume_table"></table>`);
-                    $.each(res.data.get_date, function (i, item) {
-                        $('#exampleModalCenter .append-content-modal .get-checked-lists').append(`<option value="${item.id}">${(new Date(item.start_date)).toLocaleString()} to ${(new Date(item.end_date)).toLocaleString()}</option>`)
-                    })
-                } else {
-                    Swal.fire({
-                        icon: 'warning',
-                        title: 'Oops...',
-                        text: 'film date not fount',
-                    });
+                        $.each(res.data.get_date, function (i, item) {
+                            $('#exampleModalCenter .append-content-modal .get-checked-lists').append(`<option value="${item.id}">${(new Date(item.start_date)).toLocaleString()} to ${(new Date(item.end_date)).toLocaleString()}</option>`)
+                        })
+                    } else {
+                        Swal.fire({
+                            icon: 'warning',
+                            title: 'Oops...',
+                            text: 'film date not fount',
+                        });
+                    }
                 }
-            }
-        });
+            });
+        }
     });
     $(document).on('change', '.get-checked-lists', function () {
         let date_id = $(this).val();
@@ -374,11 +411,12 @@ $(document).ready(function () {
                             return response.json();
                         }).then((res) => {
                             if (res.success) {
-                                _this.removeClass('no-checked').addClass('checked')
-                                $.alert({
-                                    'title': 'toms number:',
-                                    content: res.card
+                                _this.removeClass('no-checked').addClass('checked');
+                                Swal.fire({
+                                    title: 'ticket number',
+                                    html: `<strong>${res.card}</strong>`,
                                 });
+
                             } else {
                                 table.find('td').addClass('no-checked')
                                 $.each(app.get('checked'), function (i, item) {
@@ -435,4 +473,4 @@ $(document).ready(function () {
             },
         });
     });
-})
+});
